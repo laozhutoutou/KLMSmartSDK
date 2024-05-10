04fcfde8692cd1054563504118e4d5be4d2e760b
zhu <932312025@qq.com>
2024-05-10 17:40:28 +0800

    init framework

---
+++ Frameworks/RealSDK.framework/Headers/RealSDK.h
@@ -0,0 +1,18 @@
+//
+//  RealSDK.h
+//  RealSDK
+//
+//  Created by 朱雨 on 2024/5/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RealSDK.
+FOUNDATION_EXPORT double RealSDKVersionNumber;
+
+//! Project version string for RealSDK.
+FOUNDATION_EXPORT const unsigned char RealSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealSDK/PublicHeader.h>
+
+
