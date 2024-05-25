cedc813a7f807ee3f1422a48f99fc59452213d0d
zhu <932312025@qq.com>
2024-05-25 14:17:43 +0800

    init framework

---
+++ Frameworks/KLMSmartSDK.framework/Headers/KLMSmartSDK.h
@@ -0,0 +1,18 @@
+//
+//  KLMSmartSDK.h
+//  KLMSmartSDK
+//
+//  Created by zhu yu on 2024/5/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KLMSmartSDK.
+FOUNDATION_EXPORT double KLMSmartSDKVersionNumber;
+
+//! Project version string for KLMSmartSDK.
+FOUNDATION_EXPORT const unsigned char KLMSmartSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KLMSmartSDK/PublicHeader.h>
+
+
